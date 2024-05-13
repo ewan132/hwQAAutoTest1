3192f73b950a8757de5d13f901b6e914d057607e
Ewan132 <ewan132@yandex.ru>
2024-05-13 20:32:12 +0300

    update test Junit4

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -12,7 +12,7 @@ public class CashbackHackServiceTest {
 
         int amount = 1_000;
         int actual = service.remain(amount);
-        int expected = 1_000;
+        int expected = 0;
 
         Assert.assertEquals(expected, actual);
 
